66c5a03142c64c8950b204a6c78d2696851ae7b9
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2018-09-14 11:39:35 -0400

    Add files via upload

---
+++ 9-10/PrintTest.java
@@ -0,0 +1,15 @@
+//Carrie Krueger
+//9/10/18
+//difference between print and System.out.println();
+
+public class PrintTest {
+    public static void main(String[] args) {
+        
+        System.out.println("Hi everyone!");
+        System.out.println("It's Monday.");  //the next thing prints on the next line
+        
+        System.out.print("No ln here.");
+        System.out.print("This will be on the same line!");  // the next thing prints on the same line
+    }
+    
+}
